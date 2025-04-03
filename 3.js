1e14daef83cf9ad7d42dd22697d520bc93bb775f
katy2006 <katilenes66@gmail.com>
2025-04-03 16:47:51 +0000

    atividade comcluida

---
+++ 3.js
@@ -0,0 +1,3 @@
+let n1 = 300
+let n2 = 400
+console.log(n1+n2)
---
+++ 5.js
@@ -0,0 +1,8 @@
+var n1 = 32
+var n2 = 65
+console.log(n1>n2)
+console.log(n1>n2)
+console.log(n1=n2)
+console.log(n1<=n2)
+console.log(n1!=n2)
+console.log(n1==n2)
